ce5e77c8d023250b48b1fc78470e37fbcf820b03
danyel61 <verdugoarm@gmail.com>
2022-08-17 18:58:47 -0600

    publicaciones

--- js/netcetis.js
+++ js/netcetis.js
@@ -1,11 +1,21 @@
 $( document ).ready(function() {
- $("#like").click(function(){
+ $("#like1").click(function(){
       var likes = $("#reactions").text();
        likes++;
-       $("#reactions").text(likes);
+       $("#reactions1").text(likes);
    });
 
    $("#comment").click(function(){
        $("#comentario").toggle("slow");
+
+
+       $("#like2").click(function(){
+        var likes = $("#reactions").text();
+         likes++;
+         $("#reactions2").text(likes);
+     });
+  
+     $("#comment2").click(function(){
+         $("#comentario").toggle("slow");
     });
 });
