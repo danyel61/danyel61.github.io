df975823c77759e49811d374661599141f43b876
wage <wage@GLAKS.net>
2022-08-19 16:09:48 -0600

    Pantalla de registro

--- js/netcetis.js
+++ js/netcetis.js
@@ -1,21 +1,42 @@
 $( document ).ready(function() {
- $("#like1").click(function(){
-      var likes = $("#reactions").text();
-       likes++;
-       $("#reactions1").text(likes);
-   });
-
-   $("#comment").click(function(){
-       $("#comentario").toggle("slow");
-
-
-       $("#like2").click(function(){
+    $("#like").click(function(){
         var likes = $("#reactions").text();
-         likes++;
-         $("#reactions2").text(likes);
-     });
-  
-     $("#comment2").click(function(){
-         $("#comentario").toggle("slow");
+        likes++;
+        $("#reactions").text(likes);
     });
+
+    $("#comment").click(function(){
+        $("#comentario").toggle("slow");
+    });
+
+    $("#like2").click(function(){
+        var likes = $("#reactions2").text();
+        likes++;
+        $("#reactions2").text(likes);
+    });
+
+    $("#comment2").click(function(){
+        $("#comentario2").toggle("slow");
+    });
+
+    $("#like3").click(function(){
+        var likes = $("#reactions3").text();
+        likes++;
+        $("#reactions3").text(likes);
+    });
+
+    $("#comment3").click(function(){
+        $("#comentario3").toggle("slow");
+    });
+
+    $("#like4").click(function(){
+        var likes = $("#reactions4").text();
+        likes++;
+        $("#reactions4").text(likes);
+    });
+
+    $("#comment4").click(function(){
+        $("#comentario4").toggle("slow");
+    });
+    
 });
